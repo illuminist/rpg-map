4e7aeafc35c6b2ba4fb899882129f53d39294aa1
Witsarut Sawetkanit <illumina-ova@live.com>
2020-06-05 17:14:13 +0700

    Updates

---
+++ precache-manifest.7a57806d1b3123308548f5797a01a639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572c9f7ae32a57599fa7671db0d0e76c",
+    "url": "/rpg-map/index.html"
+  },
+  {
+    "revision": "571f8e1d29d29823980e",
+    "url": "/rpg-map/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd458db6ca2847b1552c",
+    "url": "/rpg-map/static/js/2.710fb998.chunk.js"
+  },
+  {
+    "revision": "bda6352cc1beffdb3e71c3f9415c8f1e",
+    "url": "/rpg-map/static/js/2.710fb998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571f8e1d29d29823980e",
+    "url": "/rpg-map/static/js/main.13f20ad2.chunk.js"
+  },
+  {
+    "revision": "b00a60f284c669d7555a",
+    "url": "/rpg-map/static/js/runtime-main.0e04acca.js"
+  }
+]);
